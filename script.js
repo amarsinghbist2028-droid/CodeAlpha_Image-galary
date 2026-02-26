7e637359e4a3389017339b64de61cec0910ee9fa
amarsinghbist2028-droid <amarsinghbist2028@gmail.com>
2026-02-26 10:17:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openImage(img) {
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeImage() {
+    document.getElementById("lightbox").style.display = "none";
+}
